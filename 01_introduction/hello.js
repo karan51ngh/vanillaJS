09eef26f663affd0dc1fd66fe0db4dfd3b2e2da5
karan51ngh <karansingh9535@gmail.com>
2023-01-22 16:06:21 +0530

    introduction

---
+++ 01_introduction/hello.js
@@ -0,0 +1,14 @@
+console.time('identifier_name')
+
+console.log("Hello Friend!");
+console.log([1,1,2,3,5,8])
+
+console.log({name: 'karan51ngh', systemName: 'Zephyrus'})
+console.table({name: 'karan51ngh', systemName: 'Zephyrus'})
+
+console.timeEnd('identifier_name')
+
+console.warn('im warning you!')
+
+console.assert(2 < 4, 'This is possible') // used to check if something is True or not
+console.assert(99> 100, 'This is not possible')
