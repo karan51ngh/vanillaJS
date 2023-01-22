f874e48852f7e2e2feeee2989876702fbbcce217
karan51ngh <karansingh9535@gmail.com>
2023-01-22 16:07:51 +0530

    var vs let vs const

---
+++ 03_var_let_const/index.js
@@ -0,0 +1,27 @@
+console.log('var let const')
+const author = 'karan51ngh'
+// const author = 'AbleRyd3r' can't be redeclared/updated
+var a = 45;
+var b = 'karan'
+var b = "karan51ngh"
+var c = null
+var d = undefined
+let e = 'zephyrus'
+console.log(e)
+{
+    let e = 'artios'
+    console.log(e)
+    // let e = 'atom2' can't be redeclared
+}
+console.log(b)
+
+/*
+var declared globbaly
+let locally at a block level
+
+let can be updated BUT can't be redeclares so safer.
+
+use let
+
+const can't be redeclared niether updated
+*/
