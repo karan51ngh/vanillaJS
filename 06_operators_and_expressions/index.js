7bdccd2a4051bb8e54e51ab7b09c5adab8a6b2a3
karan51ngh <karansingh9535@gmail.com>
2023-03-06 12:54:22 +0530

    operators and expressions

---
+++ 06_operators_and_expressions/index.js
@@ -0,0 +1,23 @@
+console.log("operators in JS");
+
+let a = 960;
+let b = 64;
+
+// Arithmetic operators
+
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a/b = ", a/b);
+console.log("a*b = ", a*b);
+
+console.log("a%b = ", a%b);
+console.log("a**b = ", a**b);
+
+console.log("a++ is ", a++);
+console.log("b-- is ", b--);
+
+console.log("a is ", a);
+console.log("b is ", b);
+
+console.log("--a is ", --a);
+console.log("++b is ", ++b);
