75b6a29dc6f87dd7fbb195bc4d110c61f81dd1ea
karan51ngh <karansingh9535@gmail.com>
2023-03-07 12:43:04 +0530

    escape sequence

---
+++ 11_strings_intro/index.js
@@ -0,0 +1,18 @@
+let namee = "karan51ngh";
+namee = 'karan51ngh';
+namee = `karan51ngh`;
+// we cn use " ", ' ', ` ` for declaring strings
+console.log(namee.length);
+console.log(namee[0]);
+console.log(namee[2]);
+console.log(namee[4]);
+
+let user = "karan51ngh";
+let node = "zephyrus";
+
+// template literals use `(back-ticks) it's called string interpolation
+let sentence = `${user}@${node}`
+
+console.log(sentence)
+
+// Escape Sequence characters
