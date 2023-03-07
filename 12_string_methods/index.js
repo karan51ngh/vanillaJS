32baa8fcaa5a2f0fdd1afa8c87016cf00001bb06
karan51ngh <karansingh9535@gmail.com>
2023-03-07 12:54:07 +0530

    string methods

---
+++ 12_string_methods/index.js
@@ -0,0 +1,12 @@
+let namee = "Karan51ngh";
+
+console.log(namee.length);
+console.log(namee.toUpperCase());
+console.log(namee.toLowerCase());
+console.log(namee.slice(0,5));
+console.log(namee.replace("51ngh","Singh"));
+console.log(namee.concat("@zephyrus :~ $"));
+
+
+let explstr = "  fischer    ";
+console.log(explstr.trim())
