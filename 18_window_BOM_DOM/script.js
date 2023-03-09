ef84b6916d81f21a82179181225fa2e38c2fc25a
karan51ngh <karansingh9535@gmail.com>
2023-03-09 12:54:46 +0530

    window DOM BOM

---
+++ 18_window_BOM_DOM/script.js
@@ -0,0 +1,10 @@
+//  window is a global object
+// console.log(window)
+
+// DOM: Document Object Model
+
+console.log(document)
+document.body.style.background = "blue";
+
+// BOM: Browser Object Model
+console.log(document)
