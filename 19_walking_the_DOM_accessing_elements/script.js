6a150569a3cfd996435272c72a32bd694fad9341
karan51ngh <karansingh9535@gmail.com>
2023-03-10 13:28:34 +0530

    accessing DOM elements

---
+++ 19_walking_the_DOM_accessing_elements/script.js
@@ -0,0 +1,34 @@
+console.log(document.body.firstChild) 
+/* 
+>    will return eithre #textNode if the code is:
+
+    <body>      
+        <div>
+            <p> Don't you give up!</p>
+            <span> Used to Believe.</span>
+        </div>
+        <script src = 'script.js'></script>
+    </body>
+
+>    will return eithre div if the code is:
+
+    <body><div>
+            <p> Don't you give up!</p>
+            <span> Used to Believe.</span>
+        </div>
+        <script src = 'script.js'></script>
+    </body>
+
+*/
+
+console.log(document.body.lastChild)
+console.log(document.body.childNodes) //Nodelist of all the child nodes
+let arr = Array.from(document.body.childNodes)
+
+console.log(document.body.childNodes.length)
+
+console.log(document.body.firstChild === document.body.childNodes[0])
+
+console.log(document.body.lastChild === document.body.childNodes[document.body.childNodes.length - 1])
+
+console.log(document.body.hasChildNodes()) // return True/False
