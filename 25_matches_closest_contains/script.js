407ef722e294f5e1a7d9b40cedac4e07b83e8841
karan51ngh <karansingh9535@gmail.com>
2023-03-14 12:27:11 +0530

    matches contains closest DOM

---
+++ 25_matches_closest_contains/script.js
@@ -0,0 +1,16 @@
+let id1 = document.getElementById("id1");
+console.log(id1);
+
+// matches checks if an element matches a CSS selctor
+console.log(id1.matches(".fake-class"));
+console.log(id1.matches(".box"));
+
+// closest looks for the nearest ancestor that matches the CSS selector
+let sp1 = document.getElementById("sp1");
+console.log(sp1.closest(".box"));
+console.log(sp1.closest("#sp1"));
+
+// contains True/False depending on if an element is inside a particular element or not
+console.log(id1.contains(sp1));
+console.log(sp1.contains(sp1));
+console.log(sp1.contains(id1));
