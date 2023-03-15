c098ddf0369ec9ea90d96a998e68b95e5f4df47c
karan51ngh <karansingh9535@gmail.com>
2023-03-15 11:18:46 +0530

    added error handeling

--- 34_intro_to_Callbacks/script.js
+++ 34_intro_to_Callbacks/script.js
@@ -26,15 +26,30 @@ function loadScript(src, callback)
 {
     let script = document.createElement("script");
     script.src = src;
-    script.onload = function() {
+    script.onload = function() 
+    {
         console.log("Loaded script, SRC: " + src);
-      }
-    callback(src);
+        callback(null,src);
+    }
+    
+    script.onerror = function() 
+    {
+        console.log("Error loading script " + src);
+        callback(new Error("Wrong URL"))
+    }
     document.body.appendChild(script);
+
 }
 
-function hello(name) {
-    alert('Hello Friend! '+ name);
-  }
+function hello(error,name) 
+{
+    if(error)
+    {
+        console.log("The Error is: "+error);
+        return;
+    }
+    alert('Hello Friend!, your script is: '+ name);
+}
 
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",hello);
+// loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",hello);
+loadScript("https://cdn.WRONGURLLLLLL.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js",hello);
