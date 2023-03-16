85d7b37349e6a8713b6b80c8e7f223cbbc2ab6a0
karan51ngh <karansingh9535@gmail.com>
2023-03-16 17:14:50 +0530

    more comments

--- 37_promise_then_catch/script.js
+++ 37_promise_then_catch/script.js
@@ -2,7 +2,7 @@ let p1 = new Promise(function (resolve, reject) {
     console.log("Promise is pending");
     setTimeout(() => {
             // console.log("I was a promise that's been resolved");
-            resolve(true);
+            resolve("Value passed as a string after being resolved");
             // console.log(p1);
     }, 2000)
 })
@@ -20,18 +20,22 @@ let p2 = new Promise((resolve, reject) => {
 
 // console.log(p2);
 
-// To get the value (after p1 has been completed)
+// then and catch are consumers
+
+// To get the value (after p1 has been resolved) (if we are only interested in SUCCESFUL execution)
 p1.then((value) => {
     console.log(value)
 })
 
-// To catch the errors
-p2.catch((error) => {
-        console.log("some error occurred in p2")
-})
-
-// p2.then((value)=>{
-//     console.log(value) 
-// },(error)=>{
-//     console.log(error)
+// To catch the errors (after p2 has been rejected) (if we are only interested in catching ERROR)
+// p2.catch((error) => {
+//         console.log("some error occurred in p2")
 // })
+
+
+// general syntax of promises
+p2.then((value)=>{
+    console.log(value) 
+},(error)=>{
+    console.log(error)
+})
