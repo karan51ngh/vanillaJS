7e3326ac2be80b5067ffabe57aaa154f2d2370d2
karan51ngh <karansingh9535@gmail.com>
2023-03-16 16:40:26 +0530

    added more comments

--- 36_intro_to_Promises/script.js
+++ 36_intro_to_Promises/script.js
@@ -1,8 +1,18 @@
 // PROMISE:
 /*
 Solution to callback-hell
-it is "promise of code execution"
-Code either executes or fails, in both cases the programmer is notified
+A promise is an object in JavaScript that represents the eventual completion (or failure) 
+of an asynchronous operation and its resulting value.
+
+A promise has three states:
+- pending: The initial state when the promise is created and before it has been resolved or rejected.
+- fulfilled: The state when the promise has been resolved with a value.
+- rejected: The state when the promise has been rejected with a reason (an error or exception).
+
+A promise is typically created using the Promise constructor, which takes a function (called an executor) as its argument.
+- The executor function takes two parameters: resolve and reject. 
+- The resolve function is used to fulfill the promise with a value. 
+- The reject function is used to reject the promise with a reason.
 */
 
 let promise = new Promise(function(resolve, reject) 
